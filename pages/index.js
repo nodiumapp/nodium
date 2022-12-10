563e1a6ccb31dc11047b3660e1a89d4aaf9ff2eb
Nino Kröger <nino.kroger@gmail.com>
2022-12-10 21:20:18 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -53,17 +53,29 @@ export default function Home() {
           </a>
         </div>
       </main>
+      <main style="visibility: hidden;">
+        <div id="video"></div>
+      </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/nodiumapp/nodium"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          See the code here
+        </a>
+        <p>{' '}</p>
+        <a
+          href="https://vercel.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hosted by{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
+          !
         </a>
       </footer>
     </div>
